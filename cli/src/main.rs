ced0334aff0bbd8877d9c2465bb61619701fe5aa
minwook <minkj1992@gmail.com>
2023-02-06 16:28:54 +0900

    refactor: args를free시키고, config에 clone만 담는 방법

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,20 +2,32 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
-fn find(filename: &str, target: &str) -> String {
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn find(filename: &str, _: &str) -> String {
     let mut f = File::open(filename).expect(&format!("Cannot find {filename}."));
     let mut buf = String::new();
+
     f.read_to_string(&mut buf)
         .expect(&format!("Something went wrong reading the {filename}"));
     buf
 }
 
-fn main() {
+fn get_config() -> Config {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    Config {
+        query: args[1].clone(),
+        filename: args[2].clone(),
+    }
+}
 
-    println!("Searching for '{query}' in {filename}.",);
-    println!("{}", find(&filename, query));
+fn main() {
+    let c = get_config();
+
+    println!("Searching for '{}' in {}.", c.query, c.filename);
+    println!("{}", find(c.filename.as_str(), c.query.as_str()));
 }
