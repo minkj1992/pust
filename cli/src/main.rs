0e80cd8e672de84941f9779da1e44fc4242d4021
minwook <minkj1992@gmail.com>
2023-02-06 17:47:22 +0900

    refactor: added config

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -7,6 +7,22 @@ struct Config {
     filename: String,
 }
 
+const ARGS_NUM: usize = 2;
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        // 1 represents(args[0]) current program's name
+        if args.len() != (ARGS_NUM + 1) {
+            panic!("not enough arguments, you should give {ARGS_NUM} args");
+        }
+
+        Config {
+            query: args[1].clone(),
+            filename: args[2].clone(),
+        }
+    }
+}
+
 fn find(filename: &str, _: &str) -> String {
     let mut f = File::open(filename).expect(&format!("Cannot find {filename}."));
     let mut buf = String::new();
@@ -16,17 +32,12 @@ fn find(filename: &str, _: &str) -> String {
     buf
 }
 
-fn get_config() -> Config {
-    let args: Vec<String> = env::args().collect();
-
-    Config {
-        query: args[1].clone(),
-        filename: args[2].clone(),
-    }
-}
-
 fn main() {
-    let c = get_config();
+    let c: Config;
+    {
+        let args: Vec<String> = env::args().collect();
+        c = Config::new(&args);
+    }
 
     println!("Searching for '{}' in {}.", c.query, c.filename);
     println!("{}", find(c.filename.as_str(), c.query.as_str()));
