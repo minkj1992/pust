f45ed8e0f8b9c3efdb85c501d131d929391bf01c
minwook <minkj1992@gmail.com>
2023-02-07 18:18:14 +0900

    refactor: divide main and run fn

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,44 +1,57 @@
 use std::env;
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
+const ARGS_NUM: usize = 2;
+
+// infra logic
 struct Config {
     query: String,
     filename: String,
 }
 
-const ARGS_NUM: usize = 2;
-
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         // 1 represents(args[0]) current program's name
         if args.len() != (ARGS_NUM + 1) {
-            panic!("not enough arguments, you should give {ARGS_NUM} args");
+            return Err("invalid number of args are given.");
         }
 
-        Config {
+        Ok(Config {
             query: args[1].clone(),
             filename: args[2].clone(),
-        }
+        })
     }
 }
 
-fn find(filename: &str, _: &str) -> String {
-    let mut f = File::open(filename).expect(&format!("Cannot find {filename}."));
-    let mut buf = String::new();
+// business logic
+fn run(c: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(c.filename)?;
 
-    f.read_to_string(&mut buf)
-        .expect(&format!("Something went wrong reading the {filename}"));
-    buf
+    let mut buf = String::new();
+    f.read_to_string(&mut buf)?;
+
+    println!("With text:\n{}", buf);
+    Ok(())
 }
 
+// cli logic
 fn main() {
     let c: Config;
     {
         let args: Vec<String> = env::args().collect();
-        c = Config::new(&args);
+        c = Config::build(&args).unwrap_or_else(|err| {
+            println!("Problem parsing args: '{err}'");
+            process::exit(1);
+        });
     }
 
     println!("Searching for '{}' in {}.", c.query, c.filename);
-    println!("{}", find(c.filename.as_str(), c.query.as_str()));
+
+    if let Err(e) = run(c) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
