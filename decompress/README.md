8aa82da03bda9db3bb6e4919b49f9827fd721c17
minwook <minkj1992@gmail.com>
2023-04-12 20:41:22 +0900

    feat: decompress with out_dir

--- decompress/README.md
+++ decompress/README.md
@@ -1,21 +1,30 @@
 # decompress
 
-```sh
-$ cargo run *.zip ./unzipped/
+**1. With only source file**
 
-Okay: <target/debug/decompress>
-File 0 extracted to "ethereum/"
-File 1 extracted to "ethereum/ethereum-eth-logo.png" (160579 bytes)
-File 2 extracted to "__MACOSX/ethereum/._ethereum-eth-logo.png" (524 bytes)
-File 3 extracted to "ethereum/Ethereum_Whitepaper_-_Buterin_2014.pdf" (941366 bytes)
-File 4 extracted to "__MACOSX/ethereum/._Ethereum_Whitepaper_-_Buterin_2014.pdf" (596 bytes)
-File 5 extracted to "icp/"
-File 6 extracted to "icp/internet-computer-icp-logo.png" (98375 bytes)
-File 7 extracted to "__MACOSX/icp/._internet-computer-icp-logo.png" (542 bytes)
-File 8 extracted to "icp/icp_whitepaper.pdf" (371 bytes)
-File 9 extracted to "__MACOSX/icp/._icp_whitepaper.pdf" (498 bytes)
-File 10 extracted to "bitcoin_logo.svg" (6259 bytes)
-File 11 extracted to "__MACOSX/._bitcoin_logo.svg" (487 bytes)
-File 12 extracted to "bitcoin.pdf" (184292 bytes)
-File 13 extracted to "__MACOSX/._bitcoin.pdf" (474 bytes)
+```sh
+> cargo run *.zip
+   Compiling decompress v0.1.0 (/Users/minwook/code/pust/decompress)
+    Finished dev [unoptimized + debuginfo] target(s) in 0.28s
+     Running `target/debug/decompress blockchains.zip`
+File 0 extracted to "blockchains/ethereum/"
+File 1 extracted to "blockchains/ethereum/ethereum-eth-logo.png" (160579 bytes)
+File 2 extracted to "blockchains/__MACOSX/ethereum/._ethereum-eth-logo.png" (524 bytes)
+File 3 extracted to "blockchains/ethereum/Ethereum_Whitepaper_-_Buterin_2014.pdf" (941366 bytes)
+File 4 extracted to "blockchains/__MACOSX/ethereum/._Ethereum_Whitepaper_-_Buterin_2014.pdf" (596 bytes)
+File 5 extracted to "blockchains/icp/"
+File 6 extracted to "blockchains/icp/internet-computer-icp-logo.png" (98375 bytes)
+File 7 extracted to "blockchains/__MACOSX/icp/._internet-computer-icp-logo.png" (542 bytes)
+File 8 extracted to "blockchains/icp/icp_whitepaper.pdf" (371 bytes)
+File 9 extracted to "blockchains/__MACOSX/icp/._icp_whitepaper.pdf" (498 bytes)
+File 10 extracted to "blockchains/bitcoin_logo.svg" (6259 bytes)
+File 11 extracted to "blockchains/__MACOSX/._bitcoin_logo.svg" (487 bytes)
+File 12 extracted to "blockchains/bitcoin.pdf" (184292 bytes)
+File 13 extracted to "blockchains/__MACOSX/._bitcoin.pdf" (474 bytes)
+```
+
+**2. With source and destination directories**
+
+```sh
+> cargo run *.zip <OUT_DIR>
 ```
--- decompress/src/main.rs
+++ decompress/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::{create_dir_all, set_permissions, File, Permissions};
+use std::fs;
 use std::io::copy;
 use std::path::PathBuf;
 use std::process::ExitCode;
@@ -11,22 +11,33 @@ struct Config {
 }
 
 impl Config {
+    const VALID_LEN: [usize; 2] = [2, 3];
+
     fn new(args: &Vec<String>) -> Result<Config, &str> {
-        if Config::check(&args) {
+        if !Config::is_valid(&args) {
             return Err("Must pass 3 arguments, source dir and target dir");
         }
-        Ok(Config {
-            src: PathBuf::from(&args[1].clone()),
-            dest: PathBuf::from(&args[2].clone()),
-        })
+        let src = PathBuf::from(&args[1].clone());
+
+        let dest = match args.len() {
+            2 => PathBuf::from(src.clone().file_stem().unwrap()),
+            _ => PathBuf::from(&args[2].clone()),
+        };
+
+        Ok(Config { src, dest })
     }
+
     #[inline]
-    fn check(args: &Vec<String>) -> bool {
-        return if args.len() == 2 { true } else { false };
+    fn is_valid(args: &Vec<String>) -> bool {
+        return if Config::VALID_LEN.contains(&args.len()) {
+            true
+        } else {
+            false
+        };
     }
 }
 
-fn decompress(src: PathBuf) {
+fn decompress(from: PathBuf, to: PathBuf) {
     fn check_comment(i: usize, comment: &str) {
         if !(comment.is_empty()) {
             println!("File {} comment: {}", i, comment);
@@ -38,12 +49,18 @@ fn decompress(src: PathBuf) {
         (*name).ends_with('/')
     }
 
-    let source = File::open(src).unwrap();
+    fn set_permissions(mode: u32, out_path: &PathBuf) {
+        #[cfg(unix)]
+        use std::os::unix::fs::PermissionsExt;
+        fs::set_permissions(out_path, fs::Permissions::from_mode(mode)).unwrap();
+    }
+
+    let source = fs::File::open(from).unwrap();
     let mut archive = ZipArchive::new(source).unwrap();
     for i in 0..archive.len() {
         let mut zip_file = archive.by_index(i).unwrap();
         let out_path = match zip_file.enclosed_name() {
-            Some(path) => path.to_owned(),
+            Some(path) => to.as_path().join(path).to_owned(),
             None => continue,
         };
 
@@ -51,7 +68,7 @@ fn decompress(src: PathBuf) {
 
         if is_dir(zip_file.name()) {
             println!("File {} extracted to \"{}\"", i, out_path.display());
-            create_dir_all(&out_path).unwrap();
+            fs::create_dir_all(&out_path).unwrap();
         } else {
             println!(
                 "File {} extracted to \"{}\" ({} bytes)",
@@ -62,20 +79,16 @@ fn decompress(src: PathBuf) {
 
             if let Some(parent) = out_path.parent() {
                 if !parent.exists() {
-                    create_dir_all(&parent).unwrap();
+                    fs::create_dir_all(&parent).unwrap();
                 }
             }
 
-            let mut out_file = File::create(&out_path).unwrap();
+            let mut out_file = fs::File::create(&out_path).unwrap();
             copy(&mut zip_file, &mut out_file).unwrap();
         }
 
-        #[cfg(unix)]
-        {
-            use std::os::unix::fs::PermissionsExt;
-            if let Some(mode) = zip_file.unix_mode() {
-                set_permissions(&out_path, Permissions::from_mode(mode)).unwrap();
-            }
+        if let Some(mode) = zip_file.unix_mode() {
+            set_permissions(mode, &out_path);
         }
     }
 }
@@ -91,6 +104,7 @@ fn main() -> ExitCode {
         }
     };
 
-    decompress(cfg.src);
+    fs::create_dir_all(&cfg.dest).unwrap();
+    decompress(cfg.src, cfg.dest);
     return ExitCode::SUCCESS;
 }
