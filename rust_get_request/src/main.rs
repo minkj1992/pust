51b0b03e62e58b756836c068aba65d76ec5d2aff
minwook <minkj1992@gmail.com>
2023-05-03 15:40:16 +0900

    feat: update http response with header and status

--- rust_get_request/src/main.rs
+++ rust_get_request/src/main.rs
@@ -1,11 +1,9 @@
+use std::fs::File;
 use std::io;
 use std::io::{BufWriter, Write};
 use std::path::Path;
 
-use std::fs::File;
-
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 
 const API_URL: &str = "https://jsonplaceholder.typicode.com/posts/1";
 const FILE_NAME: &str = "output.json";
@@ -18,8 +16,15 @@ struct Post {
     body: String,
     user_id: i32,
 }
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Response {
+    status: String,
+    headers: serde_json::Value,
+    body: Post,
+}
 
-fn write_json(out_dir: &Path, data: &Post) -> io::Result<()> {
+fn write_json(out_dir: &Path, data: &Response) -> io::Result<()> {
     let f = File::create(out_dir)?;
     let mut writer = BufWriter::new(f);
     serde_json::to_writer_pretty(&mut writer, data)?;
@@ -27,15 +32,18 @@ fn write_json(out_dir: &Path, data: &Post) -> io::Result<()> {
     Ok(())
 }
 
-fn request() -> Result<Post, Box<dyn std::error::Error>> {
-    let resp = reqwest::blocking::get(API_URL)?.json::<Post>()?;
-    print!("{:?}", resp);
-    Ok(resp)
+fn request() -> Result<Response, Box<dyn std::error::Error>> {
+    let res = reqwest::blocking::get(API_URL)?;
+
+    Ok(Response {
+        status: res.status().as_str().to_owned(),
+        headers: format!("{:?}", res.headers()).into(),
+        body: res.json::<Post>()?,
+    })
 }
 
 fn main() {
     let out_dir = Path::new(FILE_NAME);
-    let post = request().unwrap();
-    let _ = write_json(out_dir, &post);
-    println!("Hello, world!");
+    let res = request().unwrap();
+    let _ = write_json(out_dir, &res);
 }
